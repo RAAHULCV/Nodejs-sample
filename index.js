a755df16a825ad0e21f87150f290584e655c4114
RAAHULCV <116276016+RAAHULCV@users.noreply.github.com>
2025-04-03 14:24:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ app.get('/', (req, res) => {
     res.send(`
         <html>
             <head>
-                <title>AWS Technical Assessment-1</title>
+                <title>AWS CodeCatalyst</title>
                 <style>
                     body { display: flex; justify-content: center; align-items: center; height: 100vh; font-size: 24px; }
                 </style>
             </head>
             <body>
-                <h1>AWS Technical Assessment-1</h1>
+                <h1>AWS CodeCatalyst</h1>
             </body>
         </html>
     `);
